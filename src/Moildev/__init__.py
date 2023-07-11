065a9312d8dd215ca82fc74c57c427c971ef07d1
Heru-sp <herusyahputra55@gmail.com>
2023-07-11 14:32:17 +0800

    first commit

---
+++ src/Moildev/__init__.py
@@ -0,0 +1,2 @@
+from .Moildev import Moildev
+from .Moildev import draw_polygon
---
+++ src/my_module.py
@@ -0,0 +1,15 @@
+"""This is a very simple function in python script in V1.0 branch"""
+
+
+def print_func_test(msg):
+    """
+    This is a simple function that prints a message
+
+    Parameters:
+       msg (str): The string to print
+    """
+
+    print(msg)
+
+
+print_func_test('Hello World branch V1.0')
